55c344ed1d971126897d0e7bfb8e67fe88c5b565
Surajit Shaw <surajit.shaw@weavers-web.com>
2025-06-04 11:42:29 +0530

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by wadmin on 04/06/25.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
